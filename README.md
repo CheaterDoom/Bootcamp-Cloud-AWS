46d4b8d497a780ef71f39a0ebcad70b9af3ca3cc
Erlant Farias <54916109+CheaterDoom@users.noreply.github.com>
2023-03-30 22:48:16 -0300

    primeiro commit
    ShellScript para automatizar criação de usuarios, grupos e permissões.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Lab 01
+
+## Ambiente de densevolvimento
+   - Estou utilizando ubuntu 22.04 LTS e Virtualbox como Hipervisor.
+
+## DIRETÓRIOS:
+    /publico
+    /adm
+    /ven
+    /sec
+
+## GRUPOS(USUARIOS):
+    GRP_ADM
+        carlos
+        maria
+        joao
+    GRP_VEN
+        debora
+        sebastiana
+        roberto
+    GRP_SEC
+        josefina
+        amanda
+        rogerio
+
+
+    - Ecluir diretórios, arquivos, grupos e usuários criados anteriormente;
+    - Todo provisionamento deve ser feito em um arquivo Bash Script;
+    - O dono de todos os diretórios criados será o usuário root;
+    - Todos os usuários terão permissão total dentro do diretório publico;
+    - Os usuários de cada grupo terão permissão total dentro de seu respectivo diretório;
+    - Os usuários não poderam ter permissão de leitura, escrita e execução em diretórios de departamentos que eles não pertencem;
+    - Subir arquivo de Script criado para a sua conta no Github.
---
+++ ubuntu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+sudo su
+echo "criando diretorios"
+
+mkdir publico
+mkdir adm
+mkdir ven
+mkdir sec
+
+echo "criando grupos"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+
+echo "mudando os diretorios dos grupos"
+
+chgrp GRP_ADM /home/vagrant/adm
+chgrp GRP_VEN /home/vagrant/ven
+chgrp GRP_SEC /home/vagrant/sec
+
+
+echo "criando os usuarios"
+
+useradd carlos -c "Carlos Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -c "Maria Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -c "João Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd debora -c "Debora Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -c "Roberto Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd josefina -c "Josefina Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -c "Amanda Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio Farias" -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "alterando as permissões dos diretorios"
+
+chmod 777 /home/vagrant/publico
+chmod 770 /home/vagrant/ven
+chmod 770 /home/vagrant/sec
+chmod 770 /home/vagrant/adm
+echo "fim"
